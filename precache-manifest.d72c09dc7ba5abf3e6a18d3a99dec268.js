4a3111bc2fdb8144c545bd292c6843538c324c48
Andrei <andreic@sprybase.com>
2020-05-20 22:34:35 +0300

    Updates

---
+++ precache-manifest.d72c09dc7ba5abf3e6a18d3a99dec268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4ac241d4238c2ef96cb8e1b46c00c6",
+    "url": "/CNHomework/index.html"
+  },
+  {
+    "revision": "ae35b833a35264e3c0e3",
+    "url": "/CNHomework/static/css/main.cd05c273.chunk.css"
+  },
+  {
+    "revision": "739834b2b20ba6566668",
+    "url": "/CNHomework/static/js/2.91828784.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/CNHomework/static/js/2.91828784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae35b833a35264e3c0e3",
+    "url": "/CNHomework/static/js/main.79db2961.chunk.js"
+  },
+  {
+    "revision": "a8db696250e98306e280",
+    "url": "/CNHomework/static/js/runtime-main.d9e1d7a4.js"
+  }
+]);
